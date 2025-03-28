fe5e04284c8323075e3879e58f8b89fc83836ac2
andreas <andreasnunesmachado2016@gmail.com>
2025-03-28 00:19:41 -0300

    initial commit

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+  session_start();
+  $errors = [
+    'login' => $_SESSION['login_error'] ?? '',
+    'register' => $_SESSION['register_error'] ?? ''
+  ];
+  $activeForm = $_SESSION['active_form'] ?? 'login';
+
+  session_unset();
+
+  function showError($error){
+    return !empty($error) ? "<p class='error-message'>$error</p>" : '';
+  }
+
+  function isActiveForm($formName, $activeForm){
+    return $formName === $activeForm ? 'active' : '';
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,9 +29,10 @@
 <body>
   
   <div class="container">
-    <div class="form-box active" id="login-form">
-      <form action="">
+    <div class="form-box <?= isActiveForm('login', $activeForm); ?>" id="login-form">
+      <form action="login_register.php" method="post">
         <h2>Login</h2>
+        <?= showError($errors['login']); ?>
         <input type="email" name="email" placeholder="Email" required>
         <input type="password" name="Password" placeholder="Password" required>
         <button type="submit" name="Login">Login</button>
@@ -19,12 +40,18 @@
       </form>
     </div>
 
-    <div class="form-box" id="register-form">
-      <form action="">
+    <div class="form-box" <?= isActiveForm('register', $activeForm); ?> id="register-form">
+      <form action="login_register.php" method="post">
         <h2>Registro</h2>
+        <?= showError($errors['register']); ?>
         <input type="text" name="name" placeholder="Nome" required>
         <input type="email" name="email" placeholder="Email" required>
         <input type="password" name="Password" placeholder="Password" required>
+        <select name="role" required>
+          <option value="">--Select Role</option>
+          <option value="user">User</option>
+          <option value="admin">Admin</option>
+        </select>
         <button type="submit" name="Login">Registre-se</button>
         <p>Ja tem login? <a href="#" onclick="showForm('login-form')">Login</a></p>
       </form>
